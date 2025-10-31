076efc06fcbba02b33644e27672e32f3d346b931
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-10-31 14:47:02 -0300

    Desenvolvo o DTO e o AutoMapper no projeot funcionario

---
+++ FunciionarioDesafio.Data/AutoMapper/FuncionarioProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using FunciionarioDesafio.Data.DTO;
+using FunciionarioDesafio.Dominio.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunciionarioDesafio.Data.AutoMapper
+{
+    public class FuncionarioProfile : Profile
+    {
+        public FuncionarioProfile()
+        {
+            CreateMap<Funcionario, FuncionarioDTO>().ReverseMap();
+        }
+    }
+}
